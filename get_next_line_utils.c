fdbfb4d6b7138d21a3d9af585c406c9b1dcd8ea6
Alisa Valiullina <avaliull@f0r2s3.codam.nl>
2024-12-08 20:01:06 +0100

    commented out old stuff for now

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,47 +6,62 @@
 /*   By: avaliull <avaliull@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 15:27:38 by avaliull          #+#    #+#             */
-/*   Updated: 2024/12/08 19:07:55 by avaliull       ########   odam.nl        */
+/*   Updated: 2024/12/08 20:00:46 by avaliull       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	buff_zero(char **buff_str)
-{
-	free(*buff_str);
-	*buff_str = NULL;
-}
+//void	buff_zero(char **buff_str)
+//{
+//	free(*buff_str);
+//	*buff_str = NULL;
+//}
+//
+//ssize_t	gnl_strlen(char *str)
+//{
+//	ssize_t	len;
+//
+//	if (!str)
+//		return (0);
+//	len = 0;
+//	while (str[len] != '\0')
+//		len++;
+//	return (len);
+//}
+//
+//char	*alloc_buff(char *buff_str, char *next_read, int read_re_val)
+//{
+//	int	i;
+//
+//	i = 0;
+//	buff_str = gnl_calloc(read_re_val + 1);
+//	if (!buff_str)
+//		return (NULL);
+//	while (i < read_re_val)
+//	{
+//		buff_str[i] = *next_read++;
+//		i++;
+//	}
+//	return (buff_str);
+//}
 
-ssize_t	gnl_strlen(char *str)
-{
-	ssize_t	len;
+//void	*gnl_calloc(int size)
+//{
+//	unsigned char	*new_mem;
+//
+//	new_mem = malloc(size);
+//	if (!new_mem)
+//		return (NULL);
+//	size--;
+//	while (size >= 0)
+//	{
+//		new_mem[size] = '\0';
+//		size--;
+//	}
+//	return (new_mem);
+//}
 
-	if (!str)
-		return (0);
-	len = 0;
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
-char	*alloc_buff(char *buff_str, char *next_read, int read_re_val)
-{
-	int	i;
-
-	i = 0;
-	buff_str = gnl_calloc(read_re_val + 1);
-	if (!buff_str)
-		return (NULL);
-	while (i < read_re_val)
-	{
-		buff_str[i] = *next_read++;
-		i++;
-	}
-	return (buff_str);
-}
-
-//	EDIT THIS SO IT HAS READ VALUE AS LIMITER
 void	trim_buff(char **buffer, ssize_t nl_index)
 {
 	ssize_t	i;
@@ -64,20 +79,4 @@ void	trim_buff(char **buffer, ssize_t nl_index)
 		(*buff_str)[i] = '\0';
 		i++;
 	}
-}
-
-void	*gnl_calloc(int size)
-{
-	unsigned char	*new_mem;
-
-	new_mem = malloc(size);
-	if (!new_mem)
-		return (NULL);
-	size--;
-	while (size >= 0)
-	{
-		new_mem[size] = '\0';
-		size--;
-	}
-	return (new_mem);
 }
